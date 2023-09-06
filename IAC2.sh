c343ace0640174dc9ce49493ed466a97b8555705
Weslley Rodrigues <89474953+WeslleyRS@users.noreply.github.com>
2023-09-06 16:15:26 -0300

    Update IAC2.sh

--- IAC2.sh
+++ IAC2.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "Atualizando Parametros do Servidor..."
 apt update -y
 apt upgrade -y
 apt install apache2 -y
 apt install unzip -y
+
+echo "Atualizações Realizadas!"
+
+echo "Configurando Servidor Web..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html/
+
+echo "Servidor Web Finalizado!!"
